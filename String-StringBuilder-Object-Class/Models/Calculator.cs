2a87d13605ef686dff55cc522f91b652cb2cb727
Nihadname <nihadcoding@gmail.com>
2023-11-07 20:48:14 +0400

    Add project files.

---
+++ String-StringBuilder-Object-Class/Models/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_StringBuilder_Object_Class.Models
+{
+     class Calculator
+    {
+        public void CalculatorProgram(double num1,double num2,char operators) {
+            double counter = 0;
+            switch (operators)
+            {
+                case '+':
+                    counter=num1 + num2;
+                    Console.WriteLine($"{counter}");
+                    break;
+                case '-':
+                    counter=num1 - num2;
+                    Console.WriteLine($"{counter}");
+                    break;
+                    case '*':
+                    counter=num1 * num2;
+                    Console.WriteLine($"{counter}");
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("2ye bolmek duzgun deyil");
+                    }
+                    else
+                    {
+                        counter = num2 / num1;
+                        Console.WriteLine($"{counter}");
+                    }
+                    break;
+                    default:
+                    Console.WriteLine("operator sehv daxil olunmusdur");
+                    break;
+
+
+            }
+        
+        }
+    }
+}
---
+++ String-StringBuilder-Object-Class/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using String_StringBuilder_Object_Class.Models;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+using System.Xml;
+using static System.Net.Mime.MediaTypeNames;
+#region 1ci task
+string words = "Salamnecesen";
+//string[] result= words.Split(' ');
+//for (int i = words.Length - 1; i >= 0; i--)
+//{
+//  //  Console.WriteLine(words[i]);
+//    string JoinOne=String.Join(" ", words);
+//    Console.WriteLine(JoinOne);
+//}
+
+string ReversedOne(string value)
+{
+    StringBuilder builder = new StringBuilder();
+    for (int i = value.Length - 1; i >= 0; i--)
+    {
+        builder.Append(value[i]);
+    }
+    return builder.ToString();
+}
+Console.WriteLine(ReversedOne(words));
+#endregion
+#region 2ci task
+
+Account account = new Account();
+Console.WriteLine("enter your username:");
+string EnteredUserName=Console.ReadLine();
+Console.WriteLine("ENTER YOUR PASSSWORD:");
+string EnteredPassword=Console.ReadLine();
+account.Login(EnteredUserName,EnteredPassword);
+
+#endregion
+#region 3cu
+Factorial factorial = new Factorial();
+Console.WriteLine("reqemi daxil edin");
+int exactValue=int.Parse(Console.ReadLine());
+factorial.CalculationOfFactorial(exactValue);
+#endregion
+#region 4cu task
+Calculator calculator = new Calculator();
+Console.WriteLine("1ci reqemi daxil edin");
+double number1=double.Parse(Console.ReadLine());
+Console.WriteLine("2ci reqemi daxil edin");
+double number2=double.Parse(Console.ReadLine());
+char operators=char.Parse(Console.ReadLine());
+calculator.CalculatorProgram(number1, number2, operators);
+
+#endregion
